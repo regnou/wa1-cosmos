a45f75f6a2d0ba30670d8ded1ef6c0a134b71daa
Axel R <regnou.a@gmail.com>
2022-09-25 12:41:17 -0400

    ⬛ --

--- src/1-ALIAS/project/alias-project.sh
+++ src/1-ALIAS/project/alias-project.sh
@@ -1,7 +1,32 @@
 #!/bin/sh
+# $$$
+# $$$
+# $$$
+# PROJECT_CURRENT="$HOME/0HOME/0GARAGE/APP_blog"
+# $$$
+# $$$
+# $$$
+# %%%
+# %%% DB
+# %%%
+alias emu=' npm run emu'
+alias emu--kill=' npm run emu--kill'
 
-PROJECT_CURRENT="$HOME/0HOME/0GARAGE/APP_blog"
-
-# !!! db
-alias emu='cd $PROJECT_CURRENT && npm run emu'
-alias emu--kill='cd $PROJECT_CURRENT && npm run emu--kill'
+# &&&
+# &&& CHROME
+# &&&
+# &&& (tin non-payant)
+alias chrome--green='/Applications/Google-Chrome--green.app/Contents/MacOS/Google\ Chrome \
+              --user-data-dir=$HOME/0HOME/datadir/datadir--green'
+# &&& (tin payant)
+alias chrome--red='/Applications/Google-Chrome--red.app/Contents/MacOS/Google\ Chrome \
+              --user-data-dir="/Users/yo/0HOME/datadir/datadir--red"'
+# &&& z - launch in parallel
+# alias chrome--delete='/Applications/Google-Chrome--delete.app/Contents/MacOS/Google\ Chrome
+#             --user-data-dir="~/0HOME/datadir/datadir--delete"'
+# alias chrome--save='/Applications/Google-Chrome--save.app/Contents/MacOS/Google\ Chrome \
+#             --user-data-dir="~/0HOME/datadir/datadir--save"'
+# alias chrome--chatUntilGetPhone='/Applications/Google-Chrome--chatUntilGetPhone.app/Contents/MacOS/Google\ Chrome \
+#             --user-data-dir="~/0HOME/datadir/datadir--chatUntilGetPhone"'
+# alias chrome-firstChat='/Applications/Google-Chrome--firstChat.app/Contents/MacOS/Google\ Chrome \
+#             --user-data-dir="~/0HOME/datadir/datadir--firstChat"'
