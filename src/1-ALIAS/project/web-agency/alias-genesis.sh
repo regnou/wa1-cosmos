f724981725455863a3d38b297ff61c43cee2ec50
Axel R <regnou.a@gmail.com>
2022-11-10 21:39:13 -0400

    ⬛ --

--- src/1-ALIAS/project/web-agency/alias-genesis.sh
+++ src/1-ALIAS/project/web-agency/alias-genesis.sh
@@ -4,6 +4,7 @@
 #@@@
 
 alias spack='npm run package && cd package && npm publish --access public'
+
 alias nrs="pnpm run dev -- --open"
 
 alias genesis--create-site="node /Users/yo/0HOME/0WEBAGENCY/0GH/GENESIS/axelo/src/transformer_js/2-transfo-site.js"
