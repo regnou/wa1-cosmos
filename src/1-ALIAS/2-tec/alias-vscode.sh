dbeefaf436ff59ffa1b59a9529632d0028346183
Axel R <regnou.a@gmail.com>
2022-11-28 12:14:52 -0400

    ⬛ --

--- src/1-ALIAS/2-tec/alias-vscode.sh
+++ src/1-ALIAS/2-tec/alias-vscode.sh
@@ -3,5 +3,5 @@
 #@@@ Choose `Shell Command: Install ‘code’ command in PATH
 #@@@ git config --global core.editor "code --wait"
 
-alias vscode--save-plugin="$CODE --list-extensions > extensions-VS-25-juill-2022.list"
-alias vscode--load-plugin="cat extensions.list |% { $CODE --install-extension $_}"
+alias vscode--save-plugin="codi --list-extensions > extensions-VS-28nov2022.list"
+alias vscode--load-plugin="cat extensions.list |% { codi --install-extension $_}"
